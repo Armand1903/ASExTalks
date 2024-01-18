e594f5d1b9bd0b39a49455bec3960f0e639bb9bd
OctavianGheorghiu <127142875+OctavianGheorghiu@users.noreply.github.com>
2024-01-18 23:24:42 +0200

    Popularea bazei de date prin data.json

--- server/server.js
+++ server/server.js
@@ -106,37 +106,69 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const fs = require('fs');
 
-app.get("/sync", async (req, res, next) => {
-  try {
-    await sequelize.sync({ force: true });
-    // const sampleData = [
-    //   {
-    //     username: "first-user",
-    //     fullName: "john doe",
-    //     type: "regular-user",
-    //   },
-    //   {
-    //     username: "second-user",
-    //     fullName: "jane doe",
-    //     type: "regular-user",
-    //   },
-    //   {
-    //     username: "third-user",
-    //     fullName: "alice doe",
-    //     type: "power-user",
-    //   },
-    // ];
-    // for (const item of sampleData) {
-    //   const user = new User(item);
-    //   await user.save();
-    // }
+// app.post('/data', async (req, res) => {
+//   try {
+//     const jsonData = fs.readFileSync('data.json');
+//     const data = JSON.parse(jsonData);
 
-    res.status(201).json({ message: "conference db created" });
-  } catch (err) {
-    next(err);
-  }
-});
+//     // Sync all models to create the tables if they don't exist
+//     await sequelize.sync();
+
+//     // Insert data into the Autor table
+//     const autorIds = await Autor.bulkCreate(data.autors, { returning: ['id'] });
+//     const autorIdMap = autorIds.reduce((acc, autor, index) => {
+//       acc[`Author ${index + 1}`] = autor.id;
+//       return acc;
+//     }, {});
+
+//     // Insert data into the Organizator table
+//     await Organizator.bulkCreate(data.organizators);
+
+//     // Insert data into the Reviewer table
+//     await Reviewer.bulkCreate(data.reviewers);
+
+//     // Insert data into the Articol table
+//     await Articol.bulkCreate(data.articols);
+
+//     // Insert data into the Conferinta table
+//     const conferintaIds = await Conferinta.bulkCreate(data.conferintas, { returning: ['id'] });
+
+//     // Associate Autors and Reviewers with Conferintas
+//     for (let i = 0; i < data.conferintas.length; i++) {
+//       const conferinta = data.conferintas[i];
+//       const conferintaId = conferintaIds[i].id;
+
+//       // Associate Autors
+//       await Conferinta.update(
+//         { autors: conferinta.autors.map(author => autorIdMap[`Author ${author}`]) },
+//         { where: { id: conferintaId } }
+//       );
+
+//       // Associate Reviewers
+//       await Conferinta.update(
+//         { reviewers: conferinta.reviewers },
+//         { where: { id: conferintaId } }
+//       );
+//     }
+
+//     res.status(200).json({ message: 'Data inserted into the database' });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+// app.get("/sync", async (req, res, next) => {
+//   try {
+//     await sequelize.sync({ force: true });
+//     res.status(201).json({ message: "conference db created" });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
 /*** Requesturi
 app.get("/users", async (req, res, next) => {
   try {
