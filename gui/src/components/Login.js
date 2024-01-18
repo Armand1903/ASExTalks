db96b817167b8f293c10172e7534a8519ef36146
Armand <armandhangu2@gmail.com>
2024-01-19 01:28:43 +0200

    Conectat login prin tabele la user roles

--- gui/src/components/Login.js
+++ gui/src/components/Login.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import './Login.css';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login({ onLogin }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8080/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
       });
@@ -24,29 +23,29 @@ export default function Login({ onLogin }) {
       const data = await response.json();
 
       if (response.ok) {
-        onLogin(data.profile.role); // Pass the user role to the parent component
-        localStorage.setItem('userRole', data.profile.role);
+        onLogin(data.role); // Pass the user role to the parent component
+        localStorage.setItem("userRole", data.role);
         // Redirecționează în funcție de rol
-        switch (data.profile.role) {
-          case 'organizer':
-            navigate('/organizer');
+        switch (data.role) {
+          case "organizer":
+            navigate("/organizer");
             break;
-          case 'reviewer':
-            navigate('/reviewer');
+          case "reviewer":
+            navigate("/reviewer");
             break;
-          case 'author':
-            navigate('/author');
+          case "author":
+            navigate("/author");
             break;
           default:
-            console.error('Invalid role:', data.profile.role);
+            console.error("Invalid role:", data.role);
         }
       } else {
         // Actualizează starea pentru a afișa mesajul de eroare utilizatorului
-        setErrorMessage('Username sau parola incorecte!');
-        console.error('Login failed:', data.error);
+        setErrorMessage("Username sau parola incorecte!");
+        console.error("Login failed:", data.error);
       }
     } catch (error) {
-      console.error('Error during login:', error.message);
+      console.error("Error during login:", error.message);
     }
   };
 
@@ -54,24 +53,36 @@ export default function Login({ onLogin }) {
     <div className="text-center m-5-auto">
       <h2>Sign in to us</h2>
       <form onSubmit={handleSubmit}>
-          <p className='form-element'>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required onChange={(e) => setUsername(e.target.value)} />
-                </p>
-                <p className='form-element'>
-                    <label>Password</label>
-                    <label className="right-label">Forget password?</label>
-                    <br/>
-                    
-                  <input type="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
+        <p className="form-element">
+          <label>Username or email address</label>
+          <br />
+          <input
+            type="text"
+            name="first_name"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </p>
+        <p className="form-element">
+          <label>Password</label>
+          <label className="right-label">Forget password?</label>
+          <br />
 
-                </p>
-                <p className='form-element'>
-                    <button id="sub_btn" type="submit">Login</button>
-               </p>
+          <input
+            type="password"
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </p>
+        <p className="form-element">
+          <button id="sub_btn" type="submit">
+            Login
+          </button>
+        </p>
       </form>
       <footer>
-          <div className="error-message-container">
+        <div className="error-message-container">
           <p className="error-message">{errorMessage}</p>
         </div>
       </footer>
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const mainRouter = require("./routes/mainRouter");
 
-const { sequelize } = require("./models");
+const { sequelize, Organizator, Autor, Reviewer } = require("./models");
 
 const app = express();
 app.use(cors());
@@ -20,25 +20,68 @@ app.get("/sync", async (req, res, next) => {
   }
 });
 
+// app.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({
+//       where: {
+//         username,
+//         password,
+//       },
+//     });
+
+//     if (user) {
+//       res.json({ profile: { role: user.role } });
+//     } else {
+//       res.status(401).json({ error: "Autentificare eșuată" });
+//     }
+//   } catch (error) {
+//     console.error("Eroare la autentificare:", error.message);
+//     res.status(500).json({ error: "Eroare internă a serverului" });
+//   }
+// });
+
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({
+    const organizator = await Organizator.findOne({
       where: {
         username,
         password,
       },
+      model: Organizator,
     });
 
-    if (user) {
-      res.json({ profile: { role: user.role } });
+    const autor = await Autor.findOne({
+      where: {
+        username,
+        password,
+      },
+      model: Autor,
+    });
+
+    const reviewer = await Reviewer.findOne({
+      where: {
+        username,
+        password,
+      },
+      model: Reviewer,
+    });
+
+    if (organizator) {
+      res.json({ role: "organizer" });
+    } else if (autor) {
+      res.json({ role: "author" });
+    } else if (reviewer) {
+      res.json({ role: "reviewer" });
     } else {
-      res.status(401).json({ error: 'Autentificare eșuată' });
+      res.status(401).json({ error: "Autentificare eșuată" });
     }
   } catch (error) {
-    console.error('Eroare la autentificare:', error.message);
-    res.status(500).json({ error: 'Eroare internă a serverului' });
+    console.error("Eroare la autentificare:", error.message);
+    res.status(500).json({ error: "Eroare internă a serverului" });
   }
 });
 
@@ -47,8 +90,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "server error" });
 });
 
-
 app.listen(8080, () => {
   console.log("The server is listening!");
 });
-
